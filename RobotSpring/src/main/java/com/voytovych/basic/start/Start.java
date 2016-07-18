b0d37a79e8494e91ec4c00149f7a11d4c228a1f1
Voytovych <voytovych.ua@gmail.com>
2016-07-18 23:48:04 +0300

    Robot Spring

--- RobotSpring/src/main/java/com/voytovych/basic/start/Start.java
+++ RobotSpring/src/main/java/com/voytovych/basic/start/Start.java
@@ -7,8 +7,14 @@ import com.voytovych.basic.impls.robot.ModelT1000;
 
 public class Start {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
-		t1000.dance();
+		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		
+		Object obj = context.getBean("t1000");
+		
+		if (obj instanceof ModelT1000) {
+			ModelT1000 t1000 = (ModelT1000) obj;
+			t1000.dance();
+		}
+		
 	}
 }
