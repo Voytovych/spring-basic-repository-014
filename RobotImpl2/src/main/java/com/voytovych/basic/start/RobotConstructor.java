b11d39c55f2e0526cdf78176a7f77516700d0ee5
Voytovych <voytovych.ua@gmail.com>
2016-07-18 17:29:05 +0300

    Robot implementation

--- RobotImpl2/src/main/java/com/voytovych/basic/start/RobotConstructor.java
+++ RobotImpl2/src/main/java/com/voytovych/basic/start/RobotConstructor.java
@@ -4,20 +4,18 @@ import com.voytovych.basic.impls.sony.SonyHand;
 import com.voytovych.basic.impls.sony.SonyHead;
 import com.voytovych.basic.impls.toshiba.ToshibaLeg;
 
-
 public class RobotConstructor {
-	
-	public static void main(String[] args){
-		
-		SonyHand sonyHand = new SonyHand();		
-		ToshibaLeg toshibaLeg = new ToshibaLeg();		
+
+	public static void main(String[] args) {
+
+		SonyHand sonyHand = new SonyHand();
+		ToshibaLeg toshibaLeg = new ToshibaLeg();
 		SonyHead sonyHead = new SonyHead();
-		
+
 		Robot robot = new Robot(sonyHand, toshibaLeg, sonyHead);
-		
+
 		robot.action();
-		
-		
+
 	}
 
 }
