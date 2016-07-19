5660fa54501bc9bd3cd2f0ac9556e1ed636adcec
Voytovych <voytovych.ua@gmail.com>
2016-07-19 22:33:47 +0300

    Robot Spring

---
+++ RobotSpring2/src/main/java/com/voytovych/spring/impls/toshiba/ToshibaLeg.java
@@ -0,0 +1,11 @@
+package com.voytovych.spring.impls.toshiba;
+
+import com.voytovych.spring.interfaces.Leg;
+
+public class ToshibaLeg implements Leg {
+	
+	public void go(){
+		System.out.println("Go to Toshiba!");
+	}
+
+}
