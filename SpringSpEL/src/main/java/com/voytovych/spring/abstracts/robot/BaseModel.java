9a291f9cf58b0a3ceffc685518302db86754e6de
Voytovych <voytovych.ua@gmail.com>
2016-07-20 20:01:56 +0300

    SpringSpEL

---
+++ SpringSpEL/src/main/java/com/voytovych/spring/abstracts/robot/BaseModel.java
@@ -0,0 +1,49 @@
+package com.voytovych.spring.abstracts.robot;
+
+import com.voytovych.spring.interfaces.Hand;
+import com.voytovych.spring.interfaces.Head;
+import com.voytovych.spring.interfaces.Leg;
+import com.voytovych.spring.interfaces.Robot;
+
+public abstract class BaseModel implements Robot {
+
+	private Hand hand;
+	private Leg leg;
+	private Head head;
+
+	public BaseModel() {
+		System.out.println(this + " - BaseModel constructor()");
+	}
+
+	public BaseModel(Hand hand, Leg leg, Head head) {
+		this();
+		this.hand = hand;
+		this.leg = leg;
+		this.head = head;
+	}
+
+	public Hand getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+
+	public Leg getLeg() {
+		return leg;
+	}
+
+	public void setLeg(Leg leg) {
+		this.leg = leg;
+	}
+
+	public Head getHead() {
+		return head;
+	}
+
+	public void setHead(Head head) {
+		this.head = head;
+	}
+
+}
---
+++ SpringSpEL/src/main/java/com/voytovych/spring/main/Start.java
@@ -0,0 +1,16 @@
+package com.voytovych.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.voytovych.spring.impls.pool.T1000Pool;
+
+public class Start {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
+
+		T1000Pool t1000GoldenPool = (T1000Pool) context.getBean("t1000ColdenPool");
+		t1000GoldenPool.beginShow();
+
+	}
+}
