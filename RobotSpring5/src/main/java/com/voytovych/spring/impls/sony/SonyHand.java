add58f83a53d0c0d461a5ec4edcaba7774bbf29d
Voytovych <voytovych.ua@gmail.com>
2016-07-20 12:47:04 +0300

    Robot Spring

---
+++ RobotSpring5/src/main/java/com/voytovych/spring/impls/sony/SonyHand.java
@@ -0,0 +1,11 @@
+package com.voytovych.spring.impls.sony;
+
+import com.voytovych.spring.interfaces.Hand;
+
+public class SonyHand implements Hand {
+
+	public void catchSomething() {
+		System.out.println("Catched from Sony!!");
+	}
+
+}
---
+++ RobotSpring5/src/main/java/com/voytovych/spring/impls/sony/SonyHead.java
@@ -0,0 +1,11 @@
+package com.voytovych.spring.impls.sony;
+
+import com.voytovych.spring.interfaces.Head;
+
+public class SonyHead implements Head{
+	
+	public void calc(){
+		System.out.println("Thinking about Sony...");
+	}
+
+}
