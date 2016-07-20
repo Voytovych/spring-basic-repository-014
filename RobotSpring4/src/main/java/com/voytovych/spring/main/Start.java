598e6966181fdd0ff2421b813499881c80684de2
Voytovych <voytovych.ua@gmail.com>
2016-07-20 12:37:16 +0300

    Robot Spring

--- RobotSpring4/src/main/java/com/voytovych/spring/main/Start.java
+++ RobotSpring4/src/main/java/com/voytovych/spring/main/Start.java
@@ -9,7 +9,7 @@ public class Start {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
 		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
-		// ModelT1000 t1000Empty = (ModelT1000) context.getBean("t1000Empty");
+		ModelT1000 t1000Empty = (ModelT1000) context.getBean("t1000Empty");
 
 	}
 }
